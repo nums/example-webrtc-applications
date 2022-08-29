5e0f144aa792a1f84632d6b5f785e1756a94854a
Emmanuel Freard <emmanuel@streaminvaders.com>
2022-08-29 14:16:26 +0200

    Update gst.go

--- external/gstreamer-src/gst.go
+++ external/gstreamer-src/gst.go
@@ -59,7 +59,7 @@ func CreatePipeline(codecName string, tracks []*webrtc.TrackLocalStaticSample, p
 	//	clockRate = videoClockRate
 		
 	case "h264":
-		pipelineStr = pipelineSrc + " ! video/x-raw,format=I420 ! nvh264enc bitrate=2000 rc-mode=cbr gop-size=60 qos=true preset=low-latency-hq ! h264parse ! video/x-h264,stream-format=avc ! " + pipelineStr
+		pipelineStr = pipelineSrc + " ! video/x-raw,format=I420 ! nvh264enc bitrate=2000 rc-mode=cbr gop-size=-1 qos=true preset=low-latency-hq ! " + pipelineStr
 		clockRate = videoClockRate
 
 	case "opus":
